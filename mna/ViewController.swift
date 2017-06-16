f04d221f04077c285ab586fbf062112b4aa0ad8d
César Guadarrama <cesarguadarrama@icloud.com>
2017-06-16 11:43:51 -0500

    Initial Commit

---
+++ mna/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mna
+//
+//  Created by César Guadarrama on 6/16/17.
+//  Copyright © 2017 ibm-mx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
