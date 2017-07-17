90c181cc34a774be5386001ed7d17f49b0cbcb00
César Guadarrama <cesarguadarrama@icloud.com>
2017-07-17 13:54:20 -0500

    Added cool cell item for pieces

--- nelli/PiecesViewController.swift
+++ nelli/PiecesViewController.swift
@@ -8,28 +8,40 @@
 
 import UIKit
 
-class PiecesViewController: UIViewController {
-
+class PiecesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var piecesTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        piecesTableView.delegate = self
+        piecesTableView.dataSource = self
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
     }
-    */
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "pieceCell", for: indexPath) as! PieceTableViewCell
+        
+        cell.mainView.layer.cornerRadius = 10
+        
+        cell.pieceImageView.layer.cornerRadius = 10
+        
+        // Set shadow
+        cell.mainView.layer.shadowColor = UIColor.black.cgColor
+        cell.mainView.layer.shadowOpacity = 0.3
+        cell.mainView.layer.shadowOffset = CGSize.zero
+        cell.mainView.layer.shadowRadius = 4
+        
+        return cell
+    }
 
 }
---
+++ nelli/views/PieceTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  PieceTableViewCell.swift
+//  nelli
+//
+//  Created by César Guadarrama on 7/17/17.
+//  Copyright © 2017 César Guadarrama. All rights reserved.
+//
+
+import UIKit
+
+class PieceTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var mainView: UIView!
+    @IBOutlet weak var pieceImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
