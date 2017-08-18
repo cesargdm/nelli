c19d7934936488bda2f6821b3ad278c92ae98ba0
César Guadarrama <cesarguadarrama@icloud.com>
2017-08-18 11:42:34 -0500

    Created

--- nelli/controllers/CaptionsViewController.swift
+++ nelli/controllers/CaptionsViewController.swift
@@ -10,26 +10,17 @@ import UIKit
 
 class CaptionsViewController: UIViewController {
 
+    @IBOutlet weak var containerView: UIView!
+    var question = ""
+    var answer = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        containerView.layer.cornerRadius = 20
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func dimiss(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
